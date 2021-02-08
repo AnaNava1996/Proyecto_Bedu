33b582cd5edbf470dd93be43cde9abed4715657c
Hegar Garcia <hegargarcia@gmail.com>
2021-02-07 23:24:08 -0600

    refactor: improve readability

--- Dashboard.R
+++ Dashboard.R
@@ -1,16 +1,3 @@
-## app.R ##
-
-#install.packages("leaflet")
-#install.packages("units", dependencies = TRUE)
-
-#if (!require("shiny")) install.packages("shiny")
-#if (!require("shinydashboard")) install.packages("shinydashboard")
-#if (!require("shinythemes")) install.packages("shinythemes")
-#if (!require("tmap"))
-#install.packages("tmap",dependencies=TRUE)
-#library(tmap)
-
-
 library(shiny)
 library(shinydashboard)
 library(shinythemes)
@@ -18,20 +5,101 @@ library(googleway)
 library(ggplot2)
 library(dplyr)
 
-
 incendios <-
   na.omit(read.csv("./Data_Sets/incendios_con_ecoregiones_y_tiposdesuelo.csv"))
 
+dashboard <- tabItem(
+  tabName = "Dashboard",
+  fluidRow(
+    titlePanel("Histograma incendios por ecorregión y tipo de suelo"),
+    selectInput("x", "Seleccione el valor de X",
+                choices = names(incendios)),
+    box(plotOutput("plot1", width = 750)),
+  ),
+  box(
+    title = "Controls",
+    sliderInput("bins", "Número de observaciones:", 1, 30, 15)
+  )
+)
 
+graph <- tabItem(tabName = "graph",
+                 fluidRow(
+                   titlePanel(h3("Gráficos de dispersión")),
+                   selectInput("a", "Selecciona el valor de x", choices = names(mtcars)),
+                   selectInput("y", "Seleccione el valor de y",
+                               choices = names(mtcars)),
+                   selectInput(
+                     "z",
+                     "Selecciona la variable del grid",
+                     choices = c("cyl", "vs", "gear", "carb")
+                   ),
+                   box(plotOutput(
+                     "output_plot", height = 300, width = 460
+                   ))
+                 ))
+
+data.table <- tabItem(tabName = "data_table",
+                      fluidRow(titlePanel(h3("Data Table")),
+                               dataTableOutput ("data_table")))
+
+img <- tabItem(tabName = "img",
+               fluidRow(
+                 titlePanel(h3(
+                   "Imágen de calor para la correlación de las variables"
+                 )),
+                 img(src = "ecorregiones.png", align = "center",
+                     width = 750),
+                 img(src = "conabio_vegetacion.png", align = "center",
+                     width = 750)
+               ))
+
+map <- tabItem(tabName = "map",
+               fluidRow(
+                 dateRangeInput(
+                   "dates",
+                   label = h3("Incendios por rango de fechas"),
+                   start = "2000-11-01",
+                   end = "2019-12-31",
+                   min = NULL,
+                   max = NULL,
+                   format = "yyyy-mm-dd",
+                   startview = "month",
+                   weekstart = 0,
+                   language = "es",
+                   separator = " a "
+                   
+                 ),
+                 hr(),
+                 fluidRow(column(4, verbatimTextOutput("value")))
+               ))
+
+map2 <- tabItem(tabName = "map2",
+                fluidRow(
+                  dateInput(
+                    inputId = "fecha",
+                    label = h3("Incendios por rango de fechas"),
+                    value = "2019-06-12",
+                    min = "2000-11-01",
+                    max = "2019-12-31",
+                    format = "yyyy-mm-dd",
+                    startview = "month",
+                    weekstart = 0,
+                    language = "es",
+                    width = NULL,
+                    datesdisabled = NULL,
+                    daysofweekdisabled = NULL
+                  ),
+                  fluidRow(box(
+                    width = 6,
+                    google_mapOutput(outputId = "map")
+                  ))
+                ))
 
 ui <-
-  
   fluidPage(dashboardPage(
     skin = "purple",
-    
     dashboardHeader(title = "Proyecto, análisis de incendios en México",
                     titleWidth = 450),
-    
     dashboardSidebar(
       width = 350,
       sidebarMenu(
@@ -60,115 +128,17 @@ ui <-
           "Incendios por año",
           tabName = "tiempo",
           icon = icon("line-chart")
-        ) #este va a ser para las series de tiempo
-        
-        
+        )
       )
-      
     ),
-    
-    dashboardBody(
-      tabItems(
-        # Histograma
-        tabItem(
-          tabName = "Dashboard",
-          fluidRow(
-            titlePanel("Histograma incendios por ecorregión y tipo de suelo"),
-            selectInput("x", "Seleccione el valor de X",
-                        choices = names(incendios)),
-            
-            box(plotOutput("plot1", width = 750)),
-          ),
-          box(title = "Controls",
-              sliderInput("bins", "Number of observations:", 1, 30, 15))
-        ),
-        
-        # Dispersión
-        tabItem(
-          tabName = "graph",
-          fluidRow(
-            titlePanel(h3("Gráficos de dispersión")),
-            selectInput("a", "Selecciona el valor de x",
-                        choices = names(mtcars)),
-            selectInput("y", "Seleccione el valor de y",
-                        choices = names(mtcars)),
-            selectInput(
-              "z",
-              "Selecciona la variable del grid",
-              choices = c("cyl", "vs", "gear", "carb")
-            ),
-            box(plotOutput(
-              "output_plot", height = 300, width = 460
-            ))
-            
-          )
-        ),
-        
-        
-        
-        tabItem(tabName = "data_table",
-                fluidRow(
-                  titlePanel(h3("Data Table")),
-                  dataTableOutput ("data_table")
-                )),
-        
-        tabItem(tabName = "img",
-                fluidRow(
-                  titlePanel(h3(
-                    "Imágen de calor para la correlación de las variables"
-                  )),
-                  img(src = "ecorregiones.png", align = "center",
-                      width = 750),
-                  img(src = "conabio_vegetacion.png", align = "center",
-                      width = 750)
-                )),
-        
-        
-        tabItem(tabName = "map",
-                fluidRow(
-                  dateRangeInput(
-                    "dates",
-                    label = h3("Incendios por rango de fechas"),
-                    start = "2000-11-01",
-                    end = "2019-12-31",
-                    min = NULL,
-                    max = NULL,
-                    format = "yyyy-mm-dd",
-                    startview = "month",
-                    weekstart = 0,
-                    language = "es",
-                    separator = " a "
-                    
-                  ),
-                  
-                  hr(),
-                  fluidRow(column(4, verbatimTextOutput("value")))
-                )),
-        tabItem(tabName = "map2",
-                fluidRow(
-                  dateInput(
-                    inputId = "fecha",
-                    label = h3("Incendios por rango de fechas"),
-                    value = "2019-06-12",
-                    min = "2000-11-01",
-                    max = "2019-12-31",
-                    format = "yyyy-mm-dd",
-                    startview = "month",
-                    weekstart = 0,
-                    language = "es",
-                    width = NULL,
-                    datesdisabled = NULL,
-                    daysofweekdisabled = NULL
-                  ),
-                  
-                  
-                  fluidRow(box(
-                    width = 6,
-                    google_mapOutput(outputId = "map")
-                  ))
-                ))
-      )
-    )
+    dashboardBody(tabItems(
+      dashboard,
+      graph,
+      data.table,
+      img,
+      map,
+      map2
+    ))
   ))
 
 #De aquí en adelante es la parte que corresponde al server
@@ -225,7 +195,6 @@ server <- function(input, output) {
   
   output$map <- renderGoogle_map({
     entrada <- input$fecha
-    print(entrada)
     
     entre_fechas <-
       function(fecha) {
@@ -236,15 +205,13 @@ server <- function(input, output) {
     latitudes <- entre_fechas$latitude
     longitudes <- entre_fechas$longitudes
     
-    gmap <- google_map(
+    google_map(
       key = map_key,
       location = c(23.6345, -102.5528),
       data = entre_fechas,
       update_map_view = TRUE,
       zoom = 5
-    ) #%>%
-    
-    add_markers(gmap, lat = latitudes, lon = longitudes)
+    ) %>% add_markers(lat = latitudes, lon = longitudes)
   })
   
 }
